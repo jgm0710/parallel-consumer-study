38f13805863b9fa41a81311cc9a90c4cdc8bfd3f
jeong-gumin <jgm@paytalab.com>
2023-11-27 23:37:14 +0900

    feat : KafkaParallelListener annotation 추가

---
+++ src/main/kotlin/com/example/parallelconsumerstudy/annotation/KafkaParallelListener.kt
@@ -0,0 +1,9 @@
+package com.example.parallelconsumerstudy.annotation
+
+import org.springframework.messaging.handler.annotation.MessageMapping
+
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION, AnnotationTarget.ANNOTATION_CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@MessageMapping
+@MustBeDocumented
+annotation class KafkaParallelListener
